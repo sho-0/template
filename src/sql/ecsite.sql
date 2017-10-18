52d7d7606d22b203e4ef830dcb1443a039cc6a2b
Masahiro_Izawa <internousdev@00174045145793>
2017-10-18 16:53:45 +0900

    クレジット認証実装中です。

--- src/sql/ecsite.sql
+++ src/sql/ecsite.sql
@@ -3,41 +3,37 @@ drop database if exists ecsite;
 create database if not exists ecsite;
 use ecsite;
 
-drop table if exists login_user_transaction;
-
-create table login_user_transaction(
-id int not null primary key auto_increment,
-login_id varchar(16) unique,
-login_pass varchar(16),
-user_name varchar(50),
-insert_date datetime,
-updated_date datetime
-);
-
 drop table if exists item_info_transaction;
 
 create table item_info_transaction(
 id int not null primary key auto_increment,
 item_name varchar(30),
 item_price int,
-item_stock int,
+img varchar (100) null default null,
 insert_date datetime,
 update_date datetime
 );
 
-drop table if exists user_buy_item_transaction;
+drop table if exists credit;
 
-create table user_buy_item_transaction(
+create table credit(
 id int not null primary key auto_increment,
-item_transaction_id int,
-total_price int,
-total_count int,
-user_master_id varchar(16),
-pay varchar(30),
+name_e varchar(50),
+credit_number varchar (16),
+security_code varchar (4),
+id_number varchar (4),
 insert_date datetime,
-delete_date datetime
+update_date datetime
 );
 
+INSERT INTO item_info_transaction(item_name, item_price, img)
+VALUES
+("観光", 2000,"img/kankou.jpg"),
+("走行", 2000,"img/soukou.jpg"),
+("完全", 4000,"img/kanzeni.jpg");
 
-INSERT INTO item_info_transaction(item_name, item_price, item_stock) VALUES("ノートBook", 100, 50);
-INSERT INTO login_user_transaction(login_id, login_pass, user_name) VALUES("internous", "internous01", "test");
+INSERT INTO credit(name_e,credit_number,security_code,id_number)
+VALUES
+("sample1","1111111111111111","1111","1111"),
+("sample2","2222222222222222","2222","2222"),
+("sample3","3333333333333333","3333","3333");
